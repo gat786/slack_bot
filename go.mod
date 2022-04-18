0afb3b5590af290cd0fa64c2948d1a86b48ce35b
Ganesh Tiwari <ganesht049@gmail.com>
2022-04-18 16:36:38 +0530

    added function to send message to slack

--- go.mod
+++ go.mod
@@ -14,13 +14,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/websocket v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.8.0 // indirect
+	github.com/slack-go/slack v0.10.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
--- src/main.go
+++ src/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
 func main() {
@@ -38,6 +40,32 @@ func main() {
 			"message": "Commands data accepted",
 		})
 	})
+
+	type EventVerification struct {
+		Token     string `json:"token"`
+		Challenge string `json:"challenge"`
+		Type      string `json:"type"`
+	}
+
+	r.POST("/slack/events", func(ctx *gin.Context) {
+		jsonData, err := ctx.GetRawData()
+
+		if err != nil {
+			log.Fatalln("Error getting data from body")
+		}
+
+		var eventVerificationObject EventVerification
+
+		json.Unmarshal(jsonData, &eventVerificationObject)
+
+		ctx.String(200, eventVerificationObject.Challenge)
+	})
+
+	api := slack.New(slackBotToken)
+
+	api.PostMessage("CSBJY2Z47", slack.MsgOptionText("Some Text", false), slack.MsgOptionAttachments(slack.Attachment{
+		Text: "This is some text",
+	}))
 
 	r.Run()
 
